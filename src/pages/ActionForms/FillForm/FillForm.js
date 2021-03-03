38c0abad5c02d472061ab12b2c5c24044ecf9506
Eduard <info@edlixstudios.de>
2021-03-03 23:09:38 +0100

    Finish Layout Fill Form

--- src/pages/ActionForms/FillForm/FillForm.js
+++ src/pages/ActionForms/FillForm/FillForm.js
@@ -22,6 +22,9 @@ const FillForm = () => {
 
     const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
 
+    const handleOnClick = (e) => {
+        e.preventDefault()
+    }
 
     return(
         <div className="fillFormContainer" >
@@ -30,15 +33,15 @@ const FillForm = () => {
                 <div {...getRootProps({className: 'dropzone'})}>
                     <input {...getInputProps()} />
                     <BsPlusCircle style={{height:"30px",width:"30px",marginBottom:"15px"}} />
-                    <div>DRAG AND DROP ANY DIGITAL FILE FORMAT HERE</div>
+                    <div  >DRAG AND DROP ANY DIGITAL FILE FORMAT HERE</div>
                 </div>
             </div>
 
             <form className="fillFormContainer_form" >
                 <input className="fillFormContainer_form--input" type="text" name="name" id="name" placeholder="YOUR NAME *" />
                 <input  className="fillFormContainer_form--input"  type="email" name="email" id="email" placeholder="YOUR EMAIL ADRESS *" />
-                <textarea name="textarea" id="" rows="7" placeholder="OPTIONAL NOTES                                   DESCRIBE YOUR WORK OR WRITE NOTES                      FOR YOUR MENVINIENCE THIS WILL BE SHOWN ON            THE FINAL CERTIFICATE" ></textarea>
-                <input className="fillFormContainer_form--button" type="submit" value="proceed to checkout"/>
+                <textarea className="fillFormContainer_form--textarea" name="textarea" id="" rows="6" placeholder="OPTIONAL NOTES                                   DESCRIBE YOUR WORK OR WRITE NOTES                      FOR YOUR MENVINIENCE THIS WILL BE SHOWN ON            THE FINAL CERTIFICATE" ></textarea>
+                <input onClick={handleOnClick} className="fillFormContainer_form--button" type="submit" value="proceed to checkout"/>
             </form>
             
     
