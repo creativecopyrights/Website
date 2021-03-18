05d1a9de020a3eaff1c3cbee6b87c2c827992132
Eduard <info@edlixstudios.de>
2021-03-18 21:38:13 +0100

    file hashing + pdf creation

--- src/pages/ActionForms/Certificate/Certificate.js
+++ src/pages/ActionForms/Certificate/Certificate.js
@@ -1,15 +1,62 @@
+//PDF
+import pdfMake from "pdfmake/build/pdfmake"
+import pdfFonts from "pdfmake/build/vfs_fonts"
+
+import BackgroundImage from "../../../images/Background.png"
 
 // CSS
 import "./Certificate.css"
 
 import Placeholder from "../../../images/formular.png"
 
+pdfMake.vfs = pdfFonts.pdfMake.vfs
+
 const Certificate = () => {
 
 
     const onDownloadClick = () => {
         
+      //  pdfMake.createPdf(docType)
+      //  pdfMake.createPdf(docType).download("certification")
+      pdfMake.createPdf(docType).open()
     }
+
+    const date = new Date().toUTCString()
+    console.log(date)
+
+    var docType = {
+        background:{
+            image:"bg",
+            fit: [1500, 1500]
+        },
+
+        content:[
+            { text:"CERTIFICATE" ,style:"header"},
+            { text:date, style:"header" },
+        {
+            qr: "21619a0e2009d34091acb7fa09637b905c630f49e4633aa36032b6580c8787d8",
+            style:"qr"
+        }
+
+    ],
+    styles:{
+        header:{
+            alignment:"center",
+            color:"white",
+            fontSize:32,
+            bold:true
+        },
+        qr:{
+            alignment:"center"
+        }
+    },
+    images: {
+        bg: "https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JhZGllbnR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+      }
+    }
+
+
+
 
     return(
         <div>
@@ -27,9 +74,9 @@ const Certificate = () => {
 
 
             <div className="certificate__buttonContainer" >
-                <a href={Placeholder} download="certificate"  className="certificate--a" >
+           
                     <input className="certificate--input"  onClick={onDownloadClick} type="button" value="download"/>
-                </a>
+              
             </div>
 
         </div>
--- src/pages/ActionForms/FillForm/FillForm.js
+++ src/pages/ActionForms/FillForm/FillForm.js
@@ -1,3 +1,5 @@
+import {useEffect,useState} from "react"
+import sha256 from 'crypto-js/sha256';
 
 // Icons
 import { BsPlusCircle } from "react-icons/bs";
@@ -7,52 +9,76 @@ import "./FillForm.css"
 
 // DropZone
 import {useDropzone} from 'react-dropzone';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
-/*
-    TODO: 1 FillForm   
-    1 Filedropper
-    2 Textinput Email
-    3 Textinput Verify Email
-    TextArea
-    Button
 
-*/
 
 const FillForm = () => {
 
-    const {acceptedFiles, getRootProps, getInputProps} = useDropzone({maxFiles:1, accept:"image/jpeg, image/png, .obj, .fbx,.pdf"});
+    let hash = ""
 
-    const files = acceptedFiles.map(file => (
-        <div style={{fontSize:"1rem"}} >
-            {file.path} - {(file.size / 1000000).toFixed(2)} MB
-        </div>
-    ))
+    const [files,setFiles] = useState([])
+
+
+    useEffect(()=>{
+        console.log(hash)
+    },[hash])
+
+
+
+    function CreateHashFromFile(e) {
+
+
+        const reader = new FileReader()
+
+        reader.onload = function(){
+
+            hash = ""
+            hash = sha256(reader.result)
+            console.log("HASH " + hash)
+            setFiles(e.target.files)
+        }
+
+
+            if (e.target.files.length > 0 ){
+                reader.readAsDataURL(e.target.files[0])
+            }
+          
+        
+     
+      
+        
+        
+    }
 
     return(
         <div className="fillFormContainer" >
 
+           
             <div className="fillFormContainer__dropzone" >
-                <div {...getRootProps({className: 'dropzone'})}>
-                    <input {...getInputProps()} />
-                    {acceptedFiles.length === 0 ? 
-                                        <div>
-                                        <BsPlusCircle style={{height:"30px",width:"30px",marginBottom:"15px"}} />
-                                        <div  >DRAG AND DROP ANY DIGITAL FILE FORMAT HERE</div>
-                                    </div>
-                                    :
-                                    <div  >{files}</div>
-                    }
-
-
+            <form className="fillFormContainer_form" >
+            <div className="dropzone"  >            
+                <input type="file"  className="filepicker" onChange={CreateHashFromFile} />
+                <div className="inputOverlay" >
+                <BsPlusCircle style={{height:"30px",width:"30px",marginBottom:"15px"}} />
+                {files.length === 0 ? <div>DRAG AND DROP ANY DIGITAL FILE FORMAT HERE</div> :  <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}} >
+                    <div style={{width:"100%",wordWrap:"break-word"}} >
+                        {"Filename: " + files[0].name}
+                    </div>
+                    <br/>
+                    <div>
+                        {"File Size: " + Number(files[0].size/ 1000000).toFixed(2) +  " MB"}
+                    </div>
+                </div>}
                 </div>
+                          
+               
             </div>
 
-            <form className="fillFormContainer_form" >
                 <input className="fillFormContainer_form--input" type="text" name="name" id="name" placeholder="YOUR NAME *" />
                 <input  className="fillFormContainer_form--input"  type="email" name="email" id="email" placeholder="YOUR EMAIL ADRESS *" />
                 <textarea className="fillFormContainer_form--textarea" name="textarea" id="" rows="6" placeholder="OPTIONAL NOTES                                   DESCRIBE YOUR WORK OR WRITE NOTES                      FOR YOUR MENVINIENCE THIS WILL BE SHOWN ON            THE FINAL CERTIFICATE" ></textarea>
-                {acceptedFiles.length !== 0 ?
+                {files.length !== 0 ?
 
                                 <div className="fillFormContainer_form--button" >
                                     <Link style={{fontFamily:"Architects Daughter,cursive" ,width:"100%",height:"100%",textAlign:"center"}} to="/payment" className="link">proceed to checkout</Link>
@@ -65,7 +91,7 @@ const FillForm = () => {
 
                            
             </form>
-            
+            </div>
         </div>
     )
 }
