b8ebcbdc2be22c42327a8e7da5dd56c61bf7481d
Eduard <info@edlixstudios.de>
2021-04-11 23:06:21 +0200

    xiaomi

--- src/components/LinkGroup/LinkGroup.js
+++ src/components/LinkGroup/LinkGroup.js
@@ -11,7 +11,7 @@ const LinkGroup = () => {
     return(
         <div className="linkGroup" >
             <div className="hLine" >
-                <NavLink style={ window.location.pathname !== ("/payment" || "/certificate") ? {backgroundColor:"#84CFF6",pointerEvents:"none"}: {pointerEvents:"none"}}  exact className="linkTag first" to="/" >1 Fill Form</NavLink>
+                <NavLink style={ window.location.pathname === "/payment" ? {pointerEvents:"none"} : window.location.pathname === "/certificate" ?{pointerEvents:"none"}: {backgroundColor:"#84CFF6",pointerEvents:"none"} }  exact className="linkTag first" to="/" >1 Fill Form</NavLink>
                 <NavLink style={{pointerEvents:"none"}} exact className="linkTag" activeStyle={{backgroundColor:"#84CFF6"}}  to="/payment" >2 Payment</NavLink>
                 <NavLink style={{pointerEvents:"none"}} exact className="linkTag last" activeStyle={{backgroundColor:"#84CFF6"}}  to="/certificate" >3 Certificate</NavLink>
             </div>
